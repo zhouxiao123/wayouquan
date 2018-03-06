0977cc8635dca81071b312f7cc38bd088622cc14
zhouxiao <919486216@qq.com>
2018-03-06 23:19:23 +0800

    2018.3.6

--- pages/add_excavator/add_excavator.js
+++ pages/add_excavator/add_excavator.js
@@ -7,18 +7,36 @@ Page({
   data: {
     brand_value:0,
     version_value: 0,
+    city_value: 0,
     et_id:0,
     et_name:'',
     et:[],
     cs_id: 0,
     cs_name: '',
     cs:[],
+    city_id: 0,
+    city_name: '',
+    cityList: [],
     brand_id: 0,
     brand_name: '',
     brandList:[],
     version_id: 0,
     version_name: '',
-    versionList:[]
+    versionList:[],
+    ol_id: 0,
+    ol_name: '较好',
+    ol: [{ id: 0, name: "较好" }, { id: 1, name: "类似新车" }, { id: 2, name: "一般" }, { id: 3, name: "良好" }],
+    use_id: 0,
+    use_name: '自用设备',
+    use: [{ id: 0, name: "自用设备" }, { id: 1, name: "租赁设备" }, { id: 2, name: "用途不确定" }, { id: 3, name: "厂家测试机" }],
+    cus_id: 0,
+    cus_name: '停用待售',
+    cus: [{ id: 0, name: "停用待售" }, { id: 1, name: "正在施工" }, { id: 2, name: "不清楚" }],
+    disflag1:'block',
+    disflag2: 'none',
+    disflag3: 'none',
+    date:'',
+    files:[]
   },
 
   /**
@@ -39,9 +57,11 @@ Page({
         //console.log(res.data)
         that.setData({
           et: res.data.et,
-          cs: res.data.cs
+          cs: res.data.cs,
+          date: transDate(new Date(),2)
         })
         that.bindTypeChange({detail:{value:0}})
+        that.bindProvinceChange({ detail: { value: 22 } })
         wx.hideLoading()
       }
     })
@@ -114,8 +134,60 @@ Page({
       version_name: that.data.versionList[e.detail.value].name,
       version_value: e.detail.value
     })
+  }, bindProvinceChange:function (e) {
+    //console.log(e)
+    var that = this
+    that.setData({
+      cs_id: that.data.cs[e.detail.value].id,
+      cs_name: that.data.cs[e.detail.value].name
+    })
+    wx.request({
+      url: app.globalData.baseUrl + 'mobile/getCity',
+      data: {
+        cid: that.data.cs[e.detail.value].id
+      },
+      success: function (res) {
+        //console.log(res.data)
+        that.setData({
+          cityList: res.data,
+          city_id: res.data[0].id,
+          city_name: res.data[0].name,
+          city_value: 0
+        })
+        
+      }
+    })
+  }, bindCityChange(e) {
+    var that = this
+    that.setData({
+      city_id: that.data.cityList[e.detail.value].id,
+      city_name: that.data.cityList[e.detail.value].name,
+      city_value: e.detail.value
+    })
+  }, bindDateChange:function(e){
+    this.setData({
+      date: e.detail.value
+    })
   },
-
+  bindOldLevelChange:function(e){
+    var that = this
+    this.setData({
+      ol_id: that.data.ol[e.detail.value].id,
+      ol_name: that.data.ol[e.detail.value].name,
+    })
+  }, bindUseChange: function (e) {
+    var that = this
+    this.setData({
+      use_id: that.data.use[e.detail.value].id,
+      use_name: that.data.use[e.detail.value].name,
+    })
+  }, bindCurrentStatusChange: function (e) {
+    var that = this
+    this.setData({
+      cus_id: that.data.cus[e.detail.value].id,
+      cus_name: that.data.cus[e.detail.value].name,
+    })
+  },
   /**
    * 生命周期函数--监听页面初次渲染完成
    */
@@ -156,12 +228,59 @@ Page({
    */
   onReachBottom: function () {
   
-  },
+  },nextStep:function(){
+    this.setData({
+      disflag1:'none',
+      disflag2: 'block',
+    })
+  }, nextStep2: function () {
+    this.setData({
+      disflag2: 'none',
+      disflag3: 'block',
+    })
+  }, preStep: function () {
+    this.setData({
+      disflag2: 'none',
+      disflag1: 'block',
+    })
+  }, preStep2: function () {
+    this.setData({
+      disflag3: 'none',
+      disflag2: 'block',
+    })
+  }, submit: function () {
 
-  /**
-   * 用户点击右上角分享
-   */
-  onShareAppMessage: function () {
-  
+  }, chooseImg:function(){
+    var that = this
+    wx.chooseImage({
+      count: 5, // 默认9
+      sizeType: ['original', 'compressed'], // 可以指定是原图还是压缩图，默认二者都有
+      sourceType: ['album', 'camera'], // 可以指定来源是相册还是相机，默认二者都有
+      success: function (res) {
+        // 返回选定照片的本地文件路径列表，tempFilePath可以作为img标签的src属性显示图片
+        var tempFilePaths = res.tempFilePaths
+        that.setData({
+          files: tempFilePaths
+        })
+
+      }
+    })
   }
 })
+function transDate(mescStr, flag) {
+  var n = mescStr;
+  var date = new Date(n);
+  if (flag == 1) {
+    return date.getFullYear()
+  }
+  var Y = date.getFullYear() + '-';
+  var M = (date.getMonth() + 1 < 10 ? '0' + (date.getMonth() + 1) : date.getMonth() + 1) + '-';
+  var D = date.getDate() < 10 ? '0' + date.getDate() : date.getDate();
+  var hour = date.getHours()
+  var minute = date.getMinutes()
+  var second = date.getSeconds()
+  if (flag == 2) {
+    return (Y + M + D)
+  }
+  return (Y + M + D + ' ' + hour + ':' + minute)
+}
